b84ed781e6590634cf1509a6b3a94c79c6231c40
MarcoBaroni9 <baroni.marco.91@gmail.com>
2023-03-08 15:45:15 +0100

    add some tests

--- test/tickets_alert/graphql/user_test.exs
+++ test/tickets_alert/graphql/user_test.exs
@@ -1,9 +1,13 @@
 defmodule TicketsAlert.Graphql.UserTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
   alias TicketsAlert.Graphql.Schema
 
   alias TicketsAlert.Application.Jwt, as: JwtApplication
+  alias TicketsAlert.Application.User, as: UserApplication
+  alias Noether.Either
+
+  @user_active_identifier "6dd39b15-8e58-49d2-85b4-718e36af2c6b"
 
   @user_active_credentials %{
     "email" => "baroni.marco.91+active@gmail.com",
@@ -49,22 +53,31 @@ defmodule TicketsAlert.Graphql.UserTest do
   """
 
   setup do
-    # Explicitly get a connection before each test
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(TicketsAlert.Repo)
+    Ecto.Adapters.SQL.Sandbox.checkout(TicketsAlert.Repo)
+
+    {:ok,
+     non_authenticated_context: %{
+       current_user: nil,
+       authorization_token: nil
+     },
+     invalid_token_context: %{
+       authorization_token: "invalid_token",
+       current_user: nil
+     }}
   end
 
   describe "Login" do
-    test "succeed", _context do
+    test "succeed", context do
       assert {:ok, %{data: %{"login" => %{"__typename" => "LoginSuccess"}}}} =
                Absinthe.run(
                  @login_mutation,
                  Schema,
-                 context: nil,
+                 context: context.non_authenticated_context,
                  variables: @user_active_credentials
                )
     end
 
-    test "failed, the user is not active", _context do
+    test "failed, the user is not active", context do
       assert {:ok,
               %{
                 data: %{
@@ -77,12 +90,12 @@ defmodule TicketsAlert.Graphql.UserTest do
                Absinthe.run(
                  @login_mutation,
                  Schema,
-                 context: nil,
+                 context: context.non_authenticated_context,
                  variables: @user_not_active_credentials
                )
     end
 
-    test "failed, the user does not exist", _context do
+    test "failed, the user does not exist", context do
       assert {:ok,
               %{
                 data: %{
@@ -95,12 +108,29 @@ defmodule TicketsAlert.Graphql.UserTest do
                Absinthe.run(
                  @login_mutation,
                  Schema,
-                 context: nil,
+                 context: context.non_authenticated_context,
                  variables: @user_does_not_exist_credentials
                )
     end
 
-    test "succeed, check token validity", _context do
+    test "succeed, check token validity", context do
+      {:ok, %{data: %{"login" => %{"token" => token}}}} =
+        Absinthe.run(
+          @login_mutation,
+          Schema,
+          context: context.non_authenticated_context,
+          variables: @user_active_credentials
+        )
+
+      assert true == JwtApplication.is_valid_format?(token)
+    end
+
+    test "failed user is already logged in", context do
+      user_domain =
+        @user_active_identifier
+        |> UserApplication.get_by_identifier()
+        |> Either.unwrap()
+
       {:ok, %{data: %{"login" => %{"token" => token}}}} =
         Absinthe.run(
           @login_mutation,
@@ -109,7 +139,16 @@ defmodule TicketsAlert.Graphql.UserTest do
           variables: @user_active_credentials
         )
 
-      assert true == JwtApplication.is_valid_format?(token)
+      assert {:ok, %{data: %{"login" => %{"__typename" => "LoginFailure", "error" => "ALREADY_LOGGED_IN"}}}} =
+               Absinthe.run(
+                 @login_mutation,
+                 Schema,
+                 context: %{
+                   authorization_token: token,
+                   current_user: user_domain
+                 },
+                 variables: @user_active_credentials
+               )
     end
   end
 end
